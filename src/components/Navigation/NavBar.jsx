27ad85ebb678064889b796d111b6396c10ea8234
Ibrahim Sifat <ibsifat24@gmail.com>
2022-09-25 21:08:48 +0300

    logout

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { userLoggedOut } from "../../features/auth/authSlice";
 import Logo from "../../images/logo.png";
 const NavBar = () => {
+  const dispatch = useDispatch();
   const {
     user: { avatar },
   } = useSelector((state) => state.auth);
 
+  const logout = () => {
+    dispatch(userLoggedOut());
+    localStorage.clear();
+  };
   return (
     <div className="flex items-center flex-shrink-0 w-full h-16 px-10 bg-white bg-opacity-75">
       <img src={Logo} className="h-10 w-10" alt="" />
@@ -29,9 +35,19 @@ const NavBar = () => {
           Team
         </Link>
       </div>
-      <button className="flex items-center justify-center w-8 h-8 ml-auto overflow-hidden rounded-full cursor-pointer">
-        <img src={avatar} alt="" />
-      </button>
+      <div className="flex items-center ml-auto overflow-">
+        <img
+          src={avatar}
+          alt=""
+          className="rounded-full w-8 h-8 mr-4 cursor-pointer"
+        />
+        <span
+          className="font-bold cursor-pointer hover:text-purple-700"
+          onClick={logout}
+        >
+          Logout
+        </span>
+      </div>
     </div>
   );
 };
